8f38a073c7cbc807fc187baae1e80a341e3caf99
Anton Lukichev <anton@lukichev.pro>
2020-05-11 08:32:46 +0300

    improve test Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -3,7 +3,7 @@ import {
   render,
   cleanup,
 } from '@testing-library/react';
-
+import { themeWraper } from '../../utils/test';
 import Header from './index';
 import { HeaderColors } from '../../types';
 
@@ -11,7 +11,7 @@ describe('Header', () => {
   afterEach(cleanup);
 
   it('render Header', async () => {
-    const { findByTestId } = render(<Header />);
+    const { findByTestId } = render(themeWraper(<Header />));
     const header = await findByTestId('header');
     expect(header).toBeInTheDocument();
     expect(header.className).toMatch(/makeStyles-fixed/i);
@@ -19,21 +19,21 @@ describe('Header', () => {
   });
 
   it('should render with absolute props', async () => {
-    const { findByTestId } = render(
+    const { findByTestId } = render(themeWraper(
       <Header
         fixed={false}
       />
-    );
+    ));
     const header = await findByTestId('header');
     expect(header.className).toMatch(/makeStyles-absolute/i);
   });
 
   it('should render with white color props', async () => {
-    const { findByTestId } = render(
+    const { findByTestId } = render(themeWraper(
       <Header
         color={HeaderColors.white}
       />
-    );
+    ));
     const header = await findByTestId('header');
     expect(header.className).toMatch(/makeStyles-white/i);
   });
