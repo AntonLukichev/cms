b676455adec582e7a1461f505a93510da9156eab
Anton Lukichev <anton@lukichev.pro>
2020-05-20 10:15:59 +0300

    fix CRLF, fix Header color props

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,20 @@
 export interface DrawerProps {
-  anchor?: 'left' | 'right' | 'top' | 'bottom'; // Side from which the drawer will appear
+  anchor?:
+    |'left'
+    | 'right'
+    | 'top'
+    | 'bottom'; // Side from which the drawer will appear
   full?: boolean; // full width
 }
 
-export type HeaderColorsType =
-  | 'primary'
-  | 'secondary'
-  | 'white'
-  | 'black'
-  | 'transparent';
-
 export interface HeaderProps {
   fixed?: boolean; // position fixed/absolute
-  color?: HeaderColorsType; // color theme
+  color?:
+    | 'primary'
+    | 'secondary'
+    | 'white'
+    | 'black'
+    | 'transparent'; // color theme
   logo?: boolean; // show/not Logo
   drawer?: DrawerProps;
 }
